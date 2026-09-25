d5a3b238c56b5c7fc88d20abf4c6e436ba3b4069
sunmenglei <sunairport@outlook.com>
2026-09-25 14:38:42 +0800

    feat: 去掉注册表里对不上驱动类的空挂型号

--- unilabos/devices/community/mv_Keysight33510B/driver.py
+++
@@ -1,7 +0,0 @@
-from .KeysightAgilent_33XXX import Keysight33xxxDualChannels
-
-
-class Keysight33510B(Keysight33xxxDualChannels):
-    """
-    QCoDeS driver for the Keysight 33510B waveform generator.
-    """
